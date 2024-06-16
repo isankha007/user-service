b052238a3d58009479bbbe50d4c4c40805af458f
sankhadeepchatterjeee <sankhadeep2007@gmail.com>
2024-06-16 13:11:34 +0530

    initial commit

---
+++ src/test/java/com/sankha/userService/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sankha.userService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
